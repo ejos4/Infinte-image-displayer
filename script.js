34d9a5a01458880fb189b406dbc5d1e32b1aba18
ejos4 <erwanjos448@gmail.com>
2022-09-11 01:11:31 +0200

    Reimplementation of functions to fetch a choosen number of random images that append to the page. The loading state is updating correctly and the images are added to the container in the order of the fetches calls.

--- script.js
+++ script.js
@@ -1,48 +1,69 @@
+const randomImageURL = "https://picsum.photos/200/300.jpg";
 const container = document.getElementsByClassName('container')[0];
 const loaderElt = document.getElementById('loader');
-let lastImage = null;
 
-window.addEventListener('DOMContentLoaded', (e) => {
-    console.log('DOM loaded');
-})
+// ---------- Images loader management ---------- //
 
-function addImage(imgURL, lastLoaded) {
+function createImage(imgURL, alt){
     let newImg = document.createElement('img');
     newImg.setAttribute('src', imgURL);
-    lastImage = newImg;
-    container.append(newImg);
-    if (lastLoaded) loaderElt.style.display = "none";
+    newImg.setAttribute('alt', alt);
+    return newImg;
 }
 
-function getRandomImageURL(sourceURL){
+function fetchImage(sourceURL){
     return fetch(sourceURL)
         .then((response) => {
             return response.url;
         })
         .catch((error) => {
-            console.error("⚠ : If a \"Cross-Origin Request Blocked\" occurs, try to turn off your addblocker.");
+            console.error("Fetched image error : If a \"Cross-Origin Request Blocked\" occurs, try to turn off your addblocker.");
             return "";
         })
 }
 
-function getNRandomImage(number, sourceURL) {
-    loaderElt.style.display = "block";
+function makeImagePromise(url, index=0) {
+    let imgPromise = new Promise((resolve, reject) => {
+        let imgURL = fetchImage(url);
+        imgURL
+            .then((url) => {
+                let newImg = createImage(url, index);
+                resolve(newImg);
+            });
+    });
 
-    for(let i = 0; i < number; i++) {
-        let currentImageURL = getRandomImageURL(url);
-        currentImageURL
-            .then((value) => {
-                let last = false;
-                console.log(i);
-                if(i === (number - 1)) {
-                    console.log('last img trigger');
-                    last = true;
-                }
-                if(value.length !== 0) addImage(value, last);
-            })
-            .catch((error) => console.log(error));
-    }
+    return imgPromise;
 }
 
-let url = "https://picsum.photos/200/300.jpg";
-getNRandomImage(20, url);
+function makeImageListPromise(number, url) {
+    let imgListPromise = new Promise((resolve, reject) => {
+
+        let promiseList = new Array();
+        
+        for (let i=0; i<number; i++) { promiseList.push(makeImagePromise(url, i)) }
+        
+        Promise.all(promiseList)
+            .then((res) => {
+                console.log(`${number} image(s) were loaded.`)
+                resolve(res);
+            });
+    })
+
+    return imgListPromise;
+}
+
+function loadImages(number, url = randomImageURL) {
+    makeImageListPromise(number, url)
+        .then((imgList) => {
+            loaderElt.style.display = 'none';
+            for (let i=0; i<imgList.length; i++) { container.append(imgList[i]) }
+        });
+}
+
+// ---------- Auto reload ---------- //
+
+// To continue
+
+// ---------- Main ---------- //
+
+loadImages(20);
